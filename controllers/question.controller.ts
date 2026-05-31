d98769b21b8e315e9ad20a2be7b59d872b50070a
arnaudetitia <arnaud.etitia@gmail.com>
2026-05-31 18:49:19 +0200

    route edition question

--- controllers/question.controller.ts
+++ controllers/question.controller.ts
@@ -1,4 +1,5 @@
 import database = require("../database");
+import { QuestionVo } from "../models/question.model";
 
 export class QuestionController {
   public async getAllQuestions() {
@@ -32,5 +33,76 @@ export class QuestionController {
     `,
     );
     return result.rows;
+  }
+
+  public async updateQuestion(idQuestion: number, question: QuestionVo) {
+    const pool = database.Database.getPool();
+    await pool.query("BEGIN");
+    try {
+      await pool.query({
+        text: `
+		UPDATE tlmvpsp.questions
+		SET question = $2,
+		bonne_reponse = $3,
+		mauvaises_reponses = $4,
+		tri = $5
+		WHERE id = $1
+		`,
+        values: [
+          idQuestion,
+          question.question,
+          question.bonneReponse,
+          question.mauvaisesReponses,
+          question.tri,
+        ],
+      });
+
+      if (question.musique) {
+        await pool.query({
+          text: `
+		INSERT INTO tlmvpsp.musiques_question(id_question, musique, jouee_apres_question)
+		VALUES ($1, $2, $3)
+		ON CONFLICT (id_question)
+		DO UPDATE SET
+		musique = EXCLUDED.musique,
+		jouee_apres_question = EXCLUDED.jouee_apres_question
+		`,
+          values: [idQuestion, question.musique, question.joueeApresQuestion],
+        });
+      } else {
+        await pool.query({
+          text: `
+        DELETE FROM  tlmvpsp.musiques_question
+        WHERE id_question = $1
+        `,
+          values: [idQuestion],
+        });
+      }
+
+      if (question.aliases && question.aliases.length > 0) {
+        await pool.query({
+          text: `
+        INSERT INTO tlmvpsp.question_cash_aliases(id_question, aliases)
+        VALUES ($1, $2)
+        ON CONFLICT (id_question)
+        DO UPDATE SET
+        aliases = EXCLUDED.aliases
+        `,
+          values: [idQuestion, question.aliases],
+        });
+      } else {
+        await pool.query({
+          text: `
+        DELETE FROM  tlmvpsp.question_cash_aliases
+        WHERE id_question = $1
+        `,
+          values: [idQuestion],
+        });
+      }
+      await pool.query("COMMIT");
+    } catch (error) {
+      await pool.query("ROLLBACK");
+      throw error;
+    }
   }
 }
