1b49545def8709a5d76693bd08496880fe0419be
arnaudetitia <arnaud.etitia@gmail.com>
2026-09-15 21:23:51 +0200

    retablissement dotenv dans database.ts

--- database.ts
+++ database.ts
@@ -1,9 +1,13 @@
 import { Pool } from "pg";
+import dotenv from "dotenv";
 
 export class Database {
   private static pool: Pool;
 
   public static getPool(): Pool {
+    dotenv.config({
+      path: `environments/environment.${process.env.NODE_ENV}`,
+    });
     if (!this.pool) {
       this.pool = new Pool({
         connectionString: process.env.DATABASE_URL,
